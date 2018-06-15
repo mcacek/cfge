fcd50d6498616af3b178435ac97bb5091c10e6c7
Michael Cacek <michael.cacek@objectpartners.com>
2018-06-15 13:46:59 -0500

    Fixing linting errors

--- example/my-app/index.js
+++ example/my-app/index.js
@@ -2,15 +2,15 @@ const os = require('os');
 const path = require('path');
 
 /**
- * Profile configurations are JS files which allows for some dynamic functionality. Avoid using any non-core node 
+ * Profile configurations are JS files which allows for some dynamic functionality. Avoid using any non-core node
  * modules as there is no guarantee that they will be installed.
  */
 
 module.exports = {
-    // Displayed when listing apps
-    label: 'My App',
-    // The destination path for the live config
-    dest: path.resolve(os.homedir(), 'my-app.yml'),
-    // File extension used to filter for config targets
-    extension: 'yml',
+	// Displayed when listing apps
+	label: 'My App',
+	// The destination path for the live config
+	dest: path.resolve(os.homedir(), 'my-app.yml'),
+	// File extension used to filter for config targets
+	extension: 'yml'
 };
